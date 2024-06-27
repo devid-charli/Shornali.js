48c0c4432a114005713faaaaa0b2a05c7b2cf133
devid-charli <mustakali444888@gmail.com>
2024-06-27 17:32:22 +0600

    add

--- Sorna.js
+++ Sorna.js
@@ -1,14 +1,19 @@
 var myAge = 22
-console.log(myAge);
+console.log(typeof myAge);
 var friend = "Santo Biswas"
-console.log(friend);
+console.log(typeof friend);
 
-var myVillage = 'Magura'
+var myVillage = '1111'
 console.log(typeof myVillage);
 
 var myFather = 'Mr'
-console.log(myFather);
+console.log( typeof myFather);
 
 var myMother = "Mst"
-console.log(myMother);
+console.log( typeof myMother);
 
+var mobiNum = '01999999999'
+console.log(typeof mobiNum)
+
+var boolType = true;
+console.log( typeof boolType)
